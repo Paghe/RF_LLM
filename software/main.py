495f69ed888daaee7871ec6ca930b76e197bd9f9
Alessandro Paghera <paghe@Alessandros-MacBook-Air.local>
2024-11-17 10:30:44 +0100

    feat/DevOps done

--- software/main.py
+++ software/main.py
@@ -1,11 +1,20 @@
 
 from receiver import receiver
 from transmitter import transmitter
+import whisper
 
+def transcribe_audio(audio_path):
+    model = whisper.load_model("small")
+    result = model.transcribe(audio_path)
+    if result is None:
+        raise ValueError("AUDIO_FILE_PATH environment variable is not set")
+    print(result["text"])
 
 def main():
-    transmitter()
-    receiver()
+    # transmitter()
+    # receiver()
+    audio_path = "/app/audio1.wav"
+    transcribe_audio(audio_path)
 
 if __name__ == "__main__":
     main()
